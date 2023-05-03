8b2a7835bea85436eb306dae71133f7c24d8d40c
goeom77 <7eom14@gmail.com>
2023-05-03 22:46:56 +0900

    Feat 서울 시간으로 변경

--- src/main/java/com/example/client/netty/DataSender.java
+++ src/main/java/com/example/client/netty/DataSender.java
@@ -1,7 +1,9 @@
 package com.example.client.netty;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -27,8 +29,11 @@ public class DataSender {
 	public <T> void sendData(Channel channel, String dataType, T data) {
 		// 데이터 전송시간 ex) 2023-04-17/10:12:34.123
 		LocalDateTime currentTime = LocalDateTime.now();
+		ZoneId seoulZoneId = ZoneId.of("Asia/Seoul");
+		ZonedDateTime seoulTime = currentTime.atZone(seoulZoneId);
+		LocalDateTime seoulLocalTime = seoulTime.toLocalDateTime();
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd/HH:mm:ss");
-		String formattedDateTime = currentTime.format(formatter);
+		String formattedDateTime = seoulLocalTime.format(formatter);
 
 
 		String combinedData = clientName + " " + dataType + " " + data + " " + formattedDateTime;
