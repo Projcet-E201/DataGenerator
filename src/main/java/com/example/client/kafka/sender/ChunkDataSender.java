03d2da3c23b6b78f451b7c58cb6945dcbc2d4206
mauv2sky <mauv2sky@gmail.com>
2023-05-08 10:22:51 +0900

    Feat. kafka cluster ip 변경

--- src/main/java/com/example/client/kafka/sender/ChunkDataSender.java
+++ src/main/java/com/example/client/kafka/sender/ChunkDataSender.java
@@ -6,7 +6,6 @@ import org.apache.kafka.clients.producer.RecordMetadata;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
